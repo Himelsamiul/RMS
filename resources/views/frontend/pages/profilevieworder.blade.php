25b4eeec51e556c69a9c16bd280984382168a186
akik-hossain <akikhs00@gmail.com>
2024-05-17 16:39:27 +0600

    work on checkout page

--- resources/views/frontend/pages/profilevieworder.blade.php
+++ resources/views/frontend/pages/profilevieworder.blade.php
@@ -17,6 +17,18 @@
                     </div>
                     <h1 class="card-title">Details</h1>
                     <div id="printDiv">
+                        <div class="col-xl-8">
+                            <ul class="list-unstyled">
+                                <li class="text-muted"><i class="fas fa-circle" style="color:#84B0CA ;"></i> <span
+                                        class="fw-bold">Name:</span> {{ auth()->user()->name }}</li>
+                                <li class="text-muted"><i class="fas fa-circle" style="color:#84B0CA ;"></i> <span
+                                        class="fw-bold">Phone:</span> {{ auth()->user()->email }}</li>
+                                <li class="text-muted"><i class="fas fa-circle" style="color:#84B0CA ;"></i> <span
+                                        class="fw-bold">Phone No:</span> {{ auth()->user()->phoneno }} </li>
+                                <li class="text-muted"><i class="fas fa-circle" style="color:#84B0CA ;"></i> <span
+                                        class="fw-bold">Phone No:</span> {{ auth()->user()->address }} </li>
+                            </ul>
+                        </div>
                         <div class="table-responsive">
                             <table class="table table-bordered">
                                 <thead>
